f2dc116594bb327b888ea1049309a77f09904158
snehashankarnarayan <sneha4ssn@gmail.com>
2014-05-08 15:04:44 -0400

    Delete kill_runs.sh

--- kill_runs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ps aux | grep get_all_stats | grep python  
-kill -9 $(ps aux | grep get_all_stats | grep python | awk '{print $2}')
-ps aux | grep calculate_stats | grep python
-kill -9 $(ps aux | grep calculate_stats | grep python | awk '{print $2}')
-ps aux | grep run | grep bash 
-kill -9 $(ps aux | grep run | grep bash | awk '{print $2}')
